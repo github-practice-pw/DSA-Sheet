2b41d1332df6dfcbfcef2b02a538ac6c21e1effb
github-practice-pw <githubpractice4interview@gmail.com>
2024-12-06 14:49:36 -0800

    Day 1 - Commit

---
+++ beginner-problems/language-basics/input_output.cpp
@@ -0,0 +1,27 @@
+/*
+ * Input - Output
+ *
+ * Complete the function printNumber which takes an integer input from the user and prints it on the screen.
+ * Use:-
+
+   for C++ : cout << variable_name;
+   for Java : System.out.print();
+   for Python : print()
+   for Javascript : console.log()
+
+   Example 1
+        Input(user gives value): 7
+        Output: 7
+ */
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int x;
+
+    cin >> x;
+    cout << x;
+
+    return x;
+}
